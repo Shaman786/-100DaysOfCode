03e4d07e0cad843aec8a3f283886c8bf6d1e9007
Fayaj Hossain <fayajh@Fayajs-MacBook-Air.local>
2022-09-28 21:33:04 +0530

    First comit

---
+++ Day1/HelloWorld.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;/* using- you are going to use it 
+                       to use what? A namespace(A namespace is a declarative region that provides a scope to the identifiers (the names of types, functions, variables, etc) inside it. Namespaces are used to organize code into logical groups and to prevent name collisions that can occur especially when your code base includes multiple libraries)
+                       std: The std namespace(where features of the c++ Standard Library, such as string or vector, are declared) */
+int main()
+{
+    cout<<"Hello World."<<endl;// cout<< to write output to the screen
+    //endl: end of line('\n')
+    // (;)semicolon: to end a statement
+
+    
+}
---
+++ Day1/Operators.cpp
@@ -0,0 +1,24 @@
+ #include<iostream>
+ using namespace std;
+
+ int main(){
+//    int a = 2/5;
+//    cout<<a<<endl;
+
+//    cout<<2.0/5<<endl;
+
+   //relational operators(=,>,<,>=,<=,!=)
+//    int a =2;
+   int b=3;
+
+//    bool first =(a==b);
+//    cout<<first<<endl;
+
+  // logical operators : &&(AND),||(OR),(!)
+
+  int a = 21;
+  cout<<!a<<endl;
+   
+   
+
+ }
