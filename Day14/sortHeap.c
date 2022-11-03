0e81a7e77e66ad479f811e465cf82607b4d5d772
Shaman786 <fayazhossain88@gmail.com>
2022-11-03 19:27:27 +0530

    cleaning clutter

--- Day14/sortHeap.c
+++ Day14/sortHeap.c
@@ -3,16 +3,18 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-void swap(int *a, int *b) {
+void swap(int *a, int *b)
+{
     int temp = *a;
     *a = *b;
     *b = temp;
 }
 
-void heapify(int arr[], int n, int i) {
+void heapify(int arr[], int n, int i)
+{
     int largest = i;
-    int l = 2*i + 1;
-    int r = 2*i + 2;
+    int l = 2 * i + 1;
+    int r = 2 * i + 2;
 
     if (l < n && arr[l] > arr[largest])
         largest = l;
@@ -20,29 +22,34 @@ void heapify(int arr[], int n, int i) {
     if (r < n && arr[r] > arr[largest])
         largest = r;
 
-    if (largest != i) {
+    if (largest != i)
+    {
         swap(&arr[i], &arr[largest]);
         heapify(arr, n, largest);
     }
 }
 
-void heapSort(int arr[], int n) {
+void heapSort(int arr[], int n)
+{
     for (int i = n / 2 - 1; i >= 0; i--)
         heapify(arr, n, i);
 
-    for (int i = n - 1; i >= 0; i--) {
+    for (int i = n - 1; i >= 0; i--)
+    {
         swap(&arr[0], &arr[i]);
         heapify(arr, i, 0);
     }
 }
 
-void printArray(int arr[], int n) {
+void printArray(int arr[], int n)
+{
     for (int i = 0; i < n; ++i)
         printf("%d ", arr[i]);
     printf("");
 }
 
-int main() {
+int main()
+{
     int arr[] = {12, 11, 13, 5, 6, 7};
     int n = sizeof(arr) / sizeof(arr[0]);
 
@@ -51,8 +58,3 @@ int main() {
     printf("Sorted array is ");
     printArray(arr, n);
 }
-
-
-
-
-
--- Day14/sortHeap.cpp
+++ Day14/sortHeap.cpp
@@ -1,7 +1,8 @@
-// heapsort
+// Implimenting heap sort
 
 #include <bits/stdc++.h>
 using namespace std;
+int main()
 {
     int n;
     cin >> n;
@@ -10,63 +11,18 @@ using namespace std;
     {
         cin >> arr[i];
     }
-    for (int i = 1; i < n; i++)
-    {
-        int childIndex = i;
-        while (childIndex > 0)
-        {
-            int parentIndex = (childIndex - 1) / 2;
-            if (arr[childIndex] < arr[parentIndex])
-            {
-                int temp = arr[childIndex];
-                arr[childIndex] = arr[parentIndex];
-                arr[parentIndex] = temp;
-            }
-            else
-            {
-                break;
-            }
-            childIndex = parentIndex;
-        }
-    }
-    int size = n;
-    while (size > 1)
-    {
-        int temp = arr[0];
-        arr[0] = arr[size - 1];
-        arr[size - 1] = temp;
-        size--;
-        int parentIndex = 0;
-        int leftChildIndex = 2 * parentIndex + 1;
-        int rightChildIndex = 2 * parentIndex + 2;
-        while (leftChildIndex < size)
-        {
-            int minIndex = parentIndex;
-            if (arr[minIndex] > arr[leftChildIndex])
-            {
-                minIndex = leftChildIndex;
-            }
-            if (rightChildIndex < size && arr[minIndex] > arr[rightChildIndex])
-            {
-                minIndex = rightChildIndex;
-            }
-            if (minIndex == parentIndex)
-            {
-                break;
-            }
-            int temp = arr[minIndex];
-            arr[minIndex] = arr[parentIndex];
-            arr[parentIndex] = temp;
-            parentIndex = minIndex;
-            leftChildIndex = 2 * parentIndex + 1;
-            rightChildIndex = 2 * parentIndex + 2;
-        }
-    }
+    make_heap(arr, arr + n);
     for (int i = 0; i < n; i++)
     {
         cout << arr[i] << " ";
     }
+    cout << endl;
+    sort_heap(arr, arr + n);
+    for (int i = 0; i < n; i++)
+    {
+        cout << arr[i] << " ";
+    }
+    cout << endl;
     return 0;
-
-} // namespace std;
+}
 
