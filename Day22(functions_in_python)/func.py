b291dd69d45999c32436e51a34fa61eec08ee155
Shaman786 <fayazhossain88@gmail.com>
2022-12-22 21:43:38 +0530

    updating repo

---
+++ Day22(functions_in_python)/func.py
@@ -0,0 +1,50 @@
+# def calculateGmean(a, b):
+#     mean = (a*b)/(a+b)
+#     print(mean)
+
+
+# def isGreater(a, b):
+#     if (a > b):
+#         print("First Number is Greater")
+#     else:
+#         print("Second Number is Greater or Equal")
+
+
+# a = 9
+# b = 8
+# calculateGmean(a, b)
+# isGreater(a, b)
+
+# c = 8
+# d = 7
+# calculateGmean(c, d)
+# isGreater(c, d)
+
+def calculateGmean(a, b):
+    mean = (a*b)/(a+b)
+    print(mean)
+
+
+def isGreater(a, b):
+    if (a > b):
+        print("First number is greater")
+    else:
+        print("Second number is greater or equal")
+
+
+def isLesser(a, b):
+    pass
+
+
+a = 9
+b = 8
+isGreater(a, b)
+calculateGmean(a, b)
+# gmean1 = (a*b)/(a+b)
+# print(gmean1)
+c = 8
+d = 74
+isGreater(c, d)
+calculateGmean(c, d)
+# gmean2 = (c*d)/(c+d)
+# print(gmean2)
