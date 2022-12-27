c4d044baeffa77d116101650d028ec3da5eb14e9
Shaman786 <fayazhossain88@gmail.com>
2022-12-27 23:44:21 +0530

    update repo

---
+++ Day23(pointers_revision)/LearnC++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string>
+using namespace std;
+int main()
+{
+    // string and getline
+    string s;
+    cin >> s;
+    cout << s;
+} // namespace std;
--- Day23(pointers_revision)/first.cpp
+++ Day23(pointers_revision)/first.cpp
@@ -1,1 +1,11 @@
 // what are pointers?
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x, y;
+    cin >> x >> y;
+    cout << "Value of x: " << x << " and y: " << y << endl;
+    return 0;
+}
