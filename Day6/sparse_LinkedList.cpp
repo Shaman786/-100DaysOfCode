dfbc7c50beedaa5f1c7257bfbd98b971c67c0025
Shaman786 <fayazhossain88@gmail.com>
2023-01-04 09:27:29 +0530

    null changes

--- Day6/sparse_LinkedList.cpp
+++ Day6/sparse_LinkedList.cpp
@@ -1,4 +1,4 @@
-/*  In linked list, each node has four fields. These four fields are defined as: 
+/*  In linked list, each node has four fields. These four fields are defined as:
 
 Row: Index of row, where non-zero element is located
 Column: Index of column, where non-zero element is located
@@ -6,23 +6,18 @@ Value: Value of the non zero element located at index – (row,column)
 Next node: Address of the next node
 */
 
-// link to the actual code https://www.geeksforgeeks.org/sparse-matrix-representation/ 
-
-
-
-
-
+// link to the actual code https://www.geeksforgeeks.org/sparse-matrix-representation/
 
 // C++ program for sparse matrix representation.
 // Using Link list
 
-#include<iostream>
+#include <iostream>
 using namespace std;
- 
+
 // Node class to represent link list
 class Node
 {
-    public:
+public:
     int row;
     int col;
     int data;
@@ -30,14 +25,14 @@ class Node
 
     Node *next;
 };
- 
+
 // Function to create new node
 void create_new_node(Node **p, int row_index,
                      int col_index, int x)
 {
     Node *temp = *p;
     Node *r;
-     
+
     // If link list is empty then
     // create first node and assign value.
     if (temp == NULL)
@@ -49,14 +44,14 @@ void create_new_node(Node **p, int row_index,
         temp->next = NULL;
         *p = temp;
     }
-     
+
     // If link list is already created
     // then append newly created node
     else
     {
-        while (temp->next != NULL)  
+        while (temp->next != NULL)
             temp = temp->next;
-             
+
         r = new Node();
         r->row = row_index;
         r->col = col_index;
@@ -65,7 +60,7 @@ void create_new_node(Node **p, int row_index,
         temp->next = r;
     }
 }
- 
+
 // Function prints contents of linked list
 // starting from start
 void printList(Node *start)
@@ -79,7 +74,7 @@ void printList(Node *start)
     }
     cout << endl;
     cout << "column_position:";
- 
+
     ptr = start;
     while (ptr != NULL)
     {
@@ -89,33 +84,33 @@ void printList(Node *start)
     cout << endl;
     cout << "Value:";
     ptr = start;
-     
+
     while (ptr != NULL)
     {
         cout << ptr->data << " ";
         ptr = ptr->next;
     }
 }
- 
+
 // Driver Code
 int main()
 {
-     
+
     // 4x5 sparse matrix
-    int sparseMatrix[4][5] = { { 0 , 0 , 3 , 0 , 4 },
-                               { 0 , 0 , 5 , 7 , 0 },
-                               { 0 , 0 , 0 , 0 , 0 },
-                               { 0 , 2 , 6 , 0 , 0 } };
-                               int n;
-                               cin>>n;
-     
+    int sparseMatrix[4][5] = {{0, 0, 3, 0, 4},
+                              {0, 0, 5, 7, 0},
+                              {0, 0, 0, 0, 0},
+                              {0, 2, 6, 0, 0}};
+    int n;
+    cin >> n;
+
     // Creating head/first node of list as NULL
     Node *first = NULL;
-    for(int i = 0; i < 4; i++)
+    for (int i = 0; i < 4; i++)
     {
-        for(int j = 0; j < 5; j++)
+        for (int j = 0; j < 5; j++)
         {
-             
+
             // Pass only those values which
             // are non - zero
             if (sparseMatrix[i][j] != 0)
@@ -124,13 +119,11 @@ int main()
         }
     }
     printList(first);
- 
+
     return 0;
 }
- 
 
-
- /*Output
-row_position:0 0 1 1 3 3 
-column_position:2 4 2 3 1 2 
+/*Output
+row_position:0 0 1 1 3 3
+column_position:2 4 2 3 1 2
 Value:3 4 5 7 2 6  */
