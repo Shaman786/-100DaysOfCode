07e523e1fb4fd11d83eb2835416b7db323c47446
Shaman786 <fayazhossain88@gmail.com>
2023-01-16 12:25:06 +0530

    arrays

---
+++ Day27/LearnArray.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //  2D array
+    // int arr[3][5];
+    // arr[1][3] = 78;
+    // cout << arr[1][2] << endl;
+
+    // cout << arr[1][3];
+
+    // string s = "Striver";
+    // int len = s.size();
+    // s[len - 1] = 'z';
+    // cout << s[len - 1] << endl;
+    // cout << "Striver" << endl;
+    // for (int i = 1; i <= 500; i = i + 1)
+    // {
+    //     cout << "Striver" << endl;
+    // }
+    // int i = 1;
+    // while (i <= 5)
+    // {
+    //     cout << "Striver" << i << endl;
+    //     i++;
+    // }
+
+    return 0;
+}
