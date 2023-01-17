659185740b0653df63d62027dcf83c90da57f58f
Shaman786 <fayazhossain88@gmail.com>
2023-01-17 13:55:40 +0530

    initial commit

---
+++ Day29/ai.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+// Hold
+void printArray()
+{
+}
+int main()
+{
+    int number[15];
+
+    cout << "Value at 0 index " << number[1] << endl;
+    cout << endl
+         << "Everything is Fine" << endl
+         << endl;
+    return 0;
+}
